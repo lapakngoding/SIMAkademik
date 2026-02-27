0aea27f68660a94ecd5d3d8f180b721e12791b9f
Lapak Ngoding <48802188+lapakngoding@users.noreply.github.com>
2026-02-27 23:22:00 +0700

    Update urls.py
    update urls teachers and students

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,7 @@
+#apps/accounts/urls.py
 from django.urls import path
 from .views import lock_screen, unlock_screen, profile_view
+from . import views
 
 app_name = 'accounts'
 
@@ -7,5 +9,12 @@ urlpatterns = [
     path('profile/', profile_view, name='profile'),
     path('lock/', lock_screen, name='lock_screen'),
     path('unlock/', unlock_screen, name='unlock_screen'),
+    path('teachers/', views.teacher_list, name='teacher_list'),
+    path('teachers/add/', views.teacher_create, name='teacher_create'),
+    path('teachers/edit/<int:pk>/', views.teacher_edit, name='teacher_edit'),
+    path('teachers/delete/<int:pk>/', views.teacher_delete, name='teacher_delete'),
+    path('students/', views.student_list, name='student_list'),
+    path('students/add/', views.student_create, name='student_create'),
+    path('students/edit/<int:pk>/', views.student_edit, name='student_edit'),
+    #path('students/delete/<int:pk>/', views.student_delete, name='student_delete'),
 ]
-
