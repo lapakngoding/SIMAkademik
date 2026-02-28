5a39948deb16583140837159e094af4068d02752
Lapak Ngoding <48802188+lapakngoding@users.noreply.github.com>
2026-02-28 23:34:01 +0700

    Update admin.py
    Update student Profile

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from .models import StudentProfile
 
 User = get_user_model()
 
 admin.site.register(User, UserAdmin)
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ('user_profile', 'kelas')
+    list_filter = ('kelas',)
+    list_editable = ('kelas',)
